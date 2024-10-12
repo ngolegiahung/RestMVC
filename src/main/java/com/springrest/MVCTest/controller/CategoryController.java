212fddd9800e3a60dcecd0545fac14faedcbfb15
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-10-12 10:07:04 +0700

    Add Category controller.

---
+++ src/main/java/com/springrest/MVCTest/controller/CategoryController.java
@@ -0,0 +1,40 @@
+package com.springrest.MVCTest.controller;
+
+import com.springrest.MVCTest.api.v1.model.CategoryDTO;
+import com.springrest.MVCTest.domain.Category;
+import com.springrest.MVCTest.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/categories/")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+
+        return new ResponseEntity<List<CategoryDTO>>(
+                categoryService.getAllCategories(), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.getCategoryByName(name), HttpStatus.OK
+        );
+    }
+}
