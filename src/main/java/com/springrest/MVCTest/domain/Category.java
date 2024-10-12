ecbd8a33653d2458cbb36e443909cdb4a60a5ce5
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-10-12 09:10:09 +0700

    Create bootstrap.

--- src/main/java/com/springrest/MVCTest/domain/Category.java
+++ src/main/java/com/springrest/MVCTest/domain/Category.java
@@ -1,11 +1,13 @@
 package com.springrest.MVCTest.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Category {
 
     @Id
